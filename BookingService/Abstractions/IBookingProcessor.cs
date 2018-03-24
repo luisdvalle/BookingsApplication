2eb5d4fe2a7af76432a7b4e05e524948c8c857bc
luisdvalle <jums.delvalle@gmail.com>
2018-03-24 19:25:03 +1100

    Adding Library to process bookings
    Including IBookingProcessor and implementation BookingProcessor

---
+++ BookingService/Abstractions/IBookingProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingService.Abstractions
+{
+    public interface IBookingProcessor<T>
+    {
+        T ProcessBooking(T bookingRequest);
+    }
+}
---
+++ BookingService/Services/BookingProcessor.cs
@@ -0,0 +1,16 @@
+﻿using BookingService.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BookingService.Services
+{
+    public class BookingProcessor : IBookingProcessor<string>
+    {
+        public string ProcessBooking(string bookingRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
