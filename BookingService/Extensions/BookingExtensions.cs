ebcaa8e5ac02043150ef28ecda3ffa2e0c670e1e
luisdvalle <jums.delvalle@gmail.com>
2018-03-26 21:19:10 +1100

    string extension method

---
+++ BookingService/Extensions/BookingExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BookingService.Extensions
+{
+    public static class BookingExtensions
+    {
+        public static Stream ToStream(this string str)
+        {
+            byte[] byteArray = Encoding.UTF8.GetBytes(str);
+            return new MemoryStream(byteArray);
+        }
+    }
+}
