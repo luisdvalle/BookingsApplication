21022781e2c1f0788ca5a9ecee79171d7286fc9c
luisdvalle <jums.delvalle@gmail.com>
2018-03-26 21:23:04 +1100

    Removing injection of dependencies in HomeController

--- BookingsProcessingSolution/Controllers/HomeController.cs
+++ BookingsProcessingSolution/Controllers/HomeController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookingService.Abstractions;
+using BookingService.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookingsProcessingSolution.Controllers
 {
     public class HomeController : Controller
-    {
+    {   
         public IActionResult Index()
         {
             return View();
